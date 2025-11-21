a2d42d2f631388f7f812d360ee0da5be7f0fb6f1
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-11-21 08:04:34 -0500

    primer commit

---
+++ src/test/java/com/riwi/H3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.H3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
